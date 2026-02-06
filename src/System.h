fa9b6dc48819b4f6581a8cba44029e18639e8ea0
Reyshyram <102749075+Reyshyram@users.noreply.github.com>
2026-02-06 18:50:50 +0100

    Feat: remove example boilerplate and add system class

---
+++ src/System.h
@@ -0,0 +1,12 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+class System {
+  public:
+    static constexpr int TOP_WIDTH     = 400;
+    static constexpr int TOP_HEIGHT    = 240;
+    static constexpr int BOTTOM_WIDTH  = 320;
+    static constexpr int BOTTOM_HEIGHT = 240;
+};
+
+#endif
