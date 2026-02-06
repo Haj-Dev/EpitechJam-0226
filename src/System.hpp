c202574762758971174c7aa5fe3bbb0aad03ea9a
Simon Dutal <simon.dutal@epitech.eu>
2026-02-06 19:18:21 +0100

    fix: what the hell

--- src/System.hpp
+++ src/System.hpp
@@ -1,12 +1,8 @@
-#ifndef SYSTEM_H
-#define SYSTEM_H
+#pragma once
 
-class System {
-  public:
+struct System {
     static constexpr int TOP_WIDTH     = 400;
     static constexpr int TOP_HEIGHT    = 240;
     static constexpr int BOTTOM_WIDTH  = 320;
     static constexpr int BOTTOM_HEIGHT = 240;
-};
-
-#endif
+}
